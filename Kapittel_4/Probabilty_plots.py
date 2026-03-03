3548d94c6b0ce2e76d6bc628c14bfc3794082a27
Ragnhild Thielemann <ragnhild.thi@gmail.com>
2026-03-03 13:15:39 +0100

    legge til bilder

--- Kapittel_4/Probabilty_plots.py
+++ Kapittel_4/Probabilty_plots.py
@@ -3,8 +3,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-
+from scipy.stats import norm
 data = []
 
 with open("data.txt","r") as fil:
@@ -22,10 +21,23 @@ for i in range(n):
     p = ((i-0.5)/n) #finner prosentilen til det ite elementet i måledataene
     prosentiler.append(p)
 
+p_array = np.asarray(prosentiler)
 
-z_verdier = [-3,-2.25,-1.32,-1.1,-0.79]
+z_prosentiler = norm.cdf(p_array) #finner de tilsvarende prosentilene for dataverdiene
+print("z-prosentiler  prosentandel") 
 
-plt.plot(z_verdier,prosentiler[:5],".")
-plt.xlabel("Stanariserte verdier for z")
-plt.ylabel("Prosentiler fra måledateene")
+for z, p in zip(z_prosentiler,p_array):
+    print(f"{z:.3f}           {p:.3f} %")
+
+#oppretter en graf med heldning 45 grader, for å se om det er en linjær sammenheng mellom prosentilene og måledataene
+x_verdier = np.linspace(0,1,100)
+y_verdier = lambda x: 0.5 + 0.5*x
+
+
+plt.plot(p_array,z_prosentiler,".")
+plt.ylabel("Stanariserte verdier for z")
+plt.xlabel("Prosentiler fra måledateene")
+plt.plot(x_verdier,y_verdier(x_verdier), label = "y=x", color = "hotpink")
+plt.legend()
 plt.title("sammenheng mellom prosentiler fra normalfordelingen og reele måledata")
+plt.show()
